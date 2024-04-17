c665c9dfeae3692f6db5acb59f4e2e6de0de2914
erickvenom99 <prevenakhtar@mail.com>
2024-04-18 00:58:23 +0200

    update task 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -14,12 +14,12 @@ def area(self):
 
 def integer_validator(self, name, value):
     """Validate a parameter as an integer.
-        Args:
-            name (str): The name of the parameter.
+    Args:
+        name (str): The name of the parameter.
             value (int): The parameter to validate.
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
+    Raises:
+       TypeError: If value is not an integer.
+       ValueError: If value is <= 0.
     """
     if type(value) != int:
         raise TypeError("{} must be an integer".format(name))
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -17,4 +17,4 @@ def __init__(self, width, height):
     self.integer_validator("width", width)
     self.__width = width
     self.integer_validator("height", height)
-    self.__height
+    self.__height = height
