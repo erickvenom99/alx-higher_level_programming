caf47ceff6ae9949c3a64ea11217a2ccac6a1471
erickvenom99 <prevenakhtar@mail.com>
2024-04-19 04:56:33 +0200

    update task 1

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    """Module defines an addition operation"""
+
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(a, b)
+    printf(f"{a} + {b} = {result}")
