db97f5157deb36ee64d8ce0fdbbe7510e580ad62
ericvenom99 <prevenakhtar@mail.com>
2024-06-20 16:50:01 +0200

    update tasks 5,6,7,8,9,10

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+exports.converter = function (base) {
+  return function (num) {
+    const digits = '0123456789ABCDEF';
+    if (num === 0) {
+      return '0';
+    }
+    let result = '';
+    while (num > 0) {
+      result = digits[num % base] + result;
+      num = Math.floor(num / base);
+    }
+    return result;
+  };
+};
---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const square1 = require('./5-square');
+
+class Square extends square1 {
+  charPrint(c) {
+    if (typeof c === 'undefined') {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += c;
+      }
+      console.log(line);
+    }
+  }
+}
+module.exports = Square;
