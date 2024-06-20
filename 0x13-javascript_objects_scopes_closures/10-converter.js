c642741e5df42bd08191bb9a11778fd8509abd1f
ericvenom99 <prevenakhtar@mail.com>
2024-06-20 20:48:05 +0200

    redo tasked 10

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,15 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  return function (num) {
-    const digits = '0123456789ABCDEF';
-    if (num === 0) {
-      return '0';
-    }
-    let result = '';
-    while (num > 0) {
-      result = digits[num % base] + result;
-      num = Math.floor(num / base);
-    }
-    return result;
-  }
+  return function (number) {
+    return number.toString(base);
+  };
 };
