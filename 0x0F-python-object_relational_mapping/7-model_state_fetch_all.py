51d9b18383927aad07cbf3c76d5595b5d70917ef
ericvenom99 <prevenakhtar@mail.com>
2024-06-24 00:10:08 +0200

    task 7 corr

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,25 +10,21 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     comm_args = sys.argv
-    if len(comm_args) != 3:
+    if len(comm_args) != 4:
         exit(1)
 
-    db_url = 'mysql+mysqldb://{}:{}@localhost/{}'
-    engine = create_engine(
-        db_url.format(
-            comm_args[1],
-            comm_args[2],
-            comm_args[3]
-        )
+    db_url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
+        comm_args[1],
+        comm_args[2],
+        comm_args[3]
     )
+    engine = create_engine(db_url)
 
     Session = sessionmaker(bind=engine)
-    Base.metadata.create_all(engine)
-
     session = Session()
     states = session.query(State).order_by(State.id).all()
 
     for state in states:
-        print("{}: {}".format(state.id, state.name))
+        print(f"{state.id}: {state.name}")
 
     session.close()
