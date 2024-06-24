70d946555b17a55be4bf0adc7b58678924cd9cdc
ericvenom99 <prevenakhtar@mail.com>
2024-06-24 04:03:42 +0200

    update advance task

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+contains the class definition of a State and
+an instance Base = declarative_base()
+"""
+from sqlalchemy import (
+    create_engine,
+    Column,
+    Integer,
+    String,
+    MetaData
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+MyMeta = MetaData()
+Base = declarative_base(metadata=MyMeta)
+
+
+class State(Base):
+    """definition of a State and an instance"""
+    __tablename__ = 'states'
+    id = Column(
+        Integer,
+        unique=True,
+        nullable=False,
+        primary_key=True
+    )
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
