29a1c33221385f837a956eaa592eee2fb71f22c0
ericvenom99 <prevenakhtar@mail.com>
2024-07-04 22:50:24 +0200

    update task1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Extrace the value of content type
-curl --silent --head $1 | grep -i "Content-Length" | awk '{print $2}'
+curl --silent --head $1 | grep -i "Content-Length" | awk '{print $2}'| tr -d '\r'
