f552f0727e766b59ab9ebe578d520855986d180b
ericvenom99 <prevenakhtar@mail.com>
2024-07-05 07:01:37 +0200

    advance 102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message
-curl -sL -X PUT -H "" 0.0.0.0:5000/catch_me
+curl -SL -X PUT -H "" 0.0.0.0:5000/catch_me
