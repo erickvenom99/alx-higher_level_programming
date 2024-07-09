f8b6bffe369f119c089f156e3e2f23659d623750
ericvenom99 <prevenakhtar@mail.com>
2024-07-09 17:50:57 +0200

    Time for an interview!

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A python script that takes two arguments and
+    prints 10 commits from most recent to
+    oldest
+"""
+import sys
+import requests
+
+repo_name = sys.argv[1]
+repo_owner = sys.argv[2]
+url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/commits"
+response = requests.get(url)
+commits = response.json()
+
+try:
+    for i in range(10):
+        sha = commits[i].get("sha")
+        author = commits[i].get("commit").get("author").get("name")
+        print(f"{sha}:{author}")
+except IndexError:
+    pass
