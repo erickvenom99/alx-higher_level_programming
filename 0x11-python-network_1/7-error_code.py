331163adf8cdcd74a9a5aff8494da8be63f4d22b
ericvenom99 <prevenakhtar@mail.com>
2024-07-09 15:51:55 +0200

    Error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Python script sends request to URL
+   Checks HTTP status codes and 
+   display the body of the response
+"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    resp = requests.get(url)
+    if resp.status_code >= 400:
+        print("Error code: {}".format(resp.status_code))
+    else:
+        print(resp.text)
