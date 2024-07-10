a5106686982fee541a3a2d9cdd098f0e409d0a22
ericvenom99 <prevenakhtar@mail.com>
2024-07-10 04:44:48 +0200

    update task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,14 +3,15 @@
    - fetches the URL
    - displays the body in tabulation
 """
-import urllib.request
+
 
 if __name__ == '__main__':
+    import urllib.request
+
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as req:
         content = req.read()
-
-    print('Body response:')
-    print("\t - type: {}".format(type(content)))
-    print("\t - content: {}".format(content))
-    print("\t - utf8 content: {}".format(content.decode('utf-8')))
+        print('Body response:')
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
