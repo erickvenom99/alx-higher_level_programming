6644b0b916482574161cbe98f44023c30cf4d52f
ericvenom99 <prevenakhtar@mail.com>
2024-07-10 04:50:57 +0200

    update task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,8 +3,9 @@
    sends a request to the URL and
    displays the value of the X-Request-Id
 """
-import urllib.request
 import sys
+import urllib.request
+
 
 if __name__ == '__main__':
     url = sys.argv[1]
