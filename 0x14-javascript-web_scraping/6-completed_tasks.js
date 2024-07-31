337a7d2f507ecea0430042a2f7f2719563102921
ericvenom99 <prevenakhtar@mail.com>
2024-07-31 13:25:41 +0200

    task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,27 +2,19 @@
 /* that computes the number of tasks completed by user id. */
 
 const request = require('request');
+const url = process.argv[2];
 
-const Url = 'https://jsonplaceholder.typicode.com/todos';
-
-request(Url, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    const todos = JSON.parse(body);
-    const completedTasksByUser = {};
-
-    todos.forEach(todo => {
-      if (todo.completed) {
-        if (!completedTasksByUser[todo.userId]) {
-          completedTasksByUser[todo.userId] = 0;
-        }
-        completedTasksByUser[todo.userId]++;
+request(url, function (error, response, body) {
+  if (error) throw error;
+  const users = {};
+  for (const task of JSON.parse(body)) {
+    if (task.completed) {
+      if (users[task.userId]) {
+        users[task.userId]++;
+      } else {
+        users[task.userId] = 1;
       }
-    });
-
-    Object.keys(completedTasksByUser).forEach(userId => {
-      console.log(`User ${userId} has completed ${completedTasksByUser[userId]} tasks.`);
-    });
-  } else {
-    console.error('Error fetching data:', error);
+    }
   }
+  console.log(users);
 });
