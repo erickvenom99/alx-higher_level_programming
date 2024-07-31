311fda29fae62579d15577125bd1be4b207d746d
ericvenom99 <prevenakhtar@mail.com>
2024-07-31 13:36:52 +0200

    task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,11 +4,11 @@
 const fs = require('fs');
 const request = require('request');
 const url = process.argv[2];
-const filePath = process.arg[3];
+const filePath = process.argv[3];
 
 request(url, function (error, response, body) {
   if (error) throw error;
-  fs.writeFile(filePath, body, 'utf8', function (err, data) {
+  fs.writeFile(filePath, body, 'utf8', function (err) {
     if (err) throw err;
   });
 });
