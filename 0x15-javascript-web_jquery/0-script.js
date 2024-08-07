8978b6e9500eb6518642d1e851af62fdc03b9844
ericvenom99 <prevenakhtar@mail.com>
2024-08-07 21:21:31 +0200

    pushed task 0-3 scrpt

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,4 @@
+/*
+A script that update the color of the header element
+*/
+document.querySelector('header').style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,4 @@
+/*
+A script that update the color of the header element
+*/
+$('header').css('color', '#FF0000');
